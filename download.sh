eed8ffdb413a7a50b37aecb19e36ceeab66d896a
SnakeA <angelconstantinides@hotmail.com>
2017-07-06 16:45:29 +0100

    changed download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-num_of_cores=32
-data_dir=cc_dataa
+NUM_OF_CORES=32
+DATA_DIR=cc_data #Name of the folder to process data
 LAZYDIR=~/lazy
 SCRIPTS=~/baselines-emnlp2016/train/scripts
 TRUECASE_LM=/home/angelconstantinides/commoncrawllm/cc.kenlm
@@ -8,20 +8,22 @@ MML_MONO_SCRIPT=/home/angelconstantinides/scripts
 NUM_OF_LINES=3809960
 NUCLE_LINES=57151
 CC_PART_LINES=380996016
+SAMPLE=true # Sample instead of using the original file 
+SAMPLE_SIZE=3810000 #3810000 - 1% or 19050000 - 5% of the original size
 
-if [ ! -d $data_dir ]; then
+if [ ! -d $DATA_DIR ]; then
    echo "Creating Directory $data_dir"
-   mkdir $data_dir
+   mkdir $DATA_DIR
 fi
 for i in {1..1}
 do
    for j in {0..9}
    do
-      if [ ! -f ./$data_dir/en.$i$j.deduped.xz ]; then
+      if [ ! -f ./$DATA_DIR/en.$i$j.deduped.xz ]; then
          echo "File en.$i$j.deduped.xz not found!"
          echo "Downloading en.$i$j.deduped.xz ....."
          echo "===================================="
-         wget -N -P ./cc_dataa/ http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/en/deduped/en.$i$j.deduped.xz &
+         wget -N -P ./$DATA_DIR/ http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/en/deduped/en.$i$j.deduped.xz &
       fi
    done
    wait
@@ -29,26 +31,37 @@ do
    #Uncompress
    for k in {0..9}
    do
-      unxz -v $data_dir/en.$i$k.deduped.xz &
+      unxz -v ./$DATA_DIR/en.$i$k.deduped.xz &
    done
    wait
    
    #Remove Compressed Files 
    for l in {0..9}
    do
-      rm ./$data_dir/en.$i$l.deduped.xz
+      rm ./$DATA_DIR/en.$i$l.deduped.xz
    #   mv en.$i$l.deduped $data_dir/.
    done
    
+   #Sample
+   if [ "$SAMPLE" = true ] ; then 
+      echo "SAMPLING ................."
+      for o in {0..9}
+      do
+         shuf -n $SAMPLE_SIZE ./$DATA_DIR/en.$i$l.deduped > ./$DATA_DIR/en.$i$l.deduped.samp
+         rm ./$DATA_DIR/en.$i$l.deduped
+         mv ./$DATA_DIR/en.$i$l.deduped.samp ./$DATA_DIR/en.$i$l.deduped
+      done
+   fi
+
    #Tokenize
    echo "TOKENIZING ................."
    for m in {0..9}
    do
-      if [ ! -f ./$data_dir/en.$i$m.deduped.tok ]; then
+      if [ ! -f ./$DATA_DIR/en.$i$m.deduped.tok ]; then
          echo "File en.$i$m.deduped.tok not found!"
          echo "Tokenizing en.$i$m.deduped ....."
          echo "===================================="
-         ~/mosesdecoder/scripts/tokenizer/tokenizer.perl -threads $num_of_cores -time < ./$data_dir/en.$i$m.deduped > $data_dir/en.$i$m.deduped.tok &
+         ~/mosesdecoder/scripts/tokenizer/tokenizer.perl -threads $NUM_OF_CORES -time < ./$DATA_DIR/en.$i$m.deduped > $DATA_DIR/en.$i$m.deduped.tok &
       fi
    done
    wait
@@ -57,12 +70,11 @@ do
    echo "TRUECASING ................."
    for n in {0..9}
    do
-      TRUECASE= $(cat ./$data_dir/en.$i$n.deduped.tok | parallel --no-notice --pipe -k -j 4 --block 1M perl $SCRIPTS/case_graph.perl --threads $num_of_cores --lm $TRUECASE_LM --decode $LAZYDIR/bin/decode > ./$data_dir/en.$i$n.deduped.lc)
-      eval $TRUECASE
-      rm ./$data_dir/en.$i$n.deduped
-      rm ./$data_dir/en.$i$n.deduped.tok
+      cat ./$DATA_DIR/en.$i$n.deduped.tok | parallel --no-notice --pipe -k -j 4 --block 1M perl $SCRIPTS/case_graph.perl --threads $NUM_OF_CORES --lm $TRUECASE_LM --decode $LAZYDIR/bin/decode > ./$DATA_DIR/en.$i$n.deduped.lc
+      #eval $TRUECASE
+      rm ./$DATA_DIR/en.$i$n.deduped
+      rm ./$DATA_DIR/en.$i$n.deduped.tok
       echo "SCORING ................."
-      MML_SCORE= $($MML_MONO_SCRIPT/mml-score_mono.perl -corpus ./$data_dir/en.$i$n.deduped.lc -query ~/mosesdecoder/bin/query | sort --parallel $num_of_cores -t $'\t' -k 2,2 -nr | head -$NUM_OF_LINES > ./sorted_score$i$n.out ) &
-      eval $MML_SCORE
+      $MML_MONO_SCRIPT/mml-score_mono.perl -corpus ./$data_dir/en.$i$n.deduped.lc -query ~/mosesdecoder/bin/query | sort --parallel $NUM_OF_CORES -t $'\t' -k 2,2 -nr | head -$NUM_OF_LINES > ./sorted_score$i$n.out &
    done
 done
