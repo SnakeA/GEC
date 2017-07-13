32264a355437f3441dbad421a4b7d1ac7d9c493f
SnakeA <angelconstantinides@hotmail.com>
2017-07-14 00:50:18 +0100

    Created Wiked Lm script

---
+++ create_wiked_lm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+NUM_OF_CORES=10
+WIKED_DIR=wiked
+MOSES_BIN=/home/angelconstantinides/mosesdecoder/bin
+MML_BI_SCRIPT=/home/angelconstantinides/scripts
+NUM_OF_LINES5=1219638
+NUM_OF_LINES10=2439276
+NUM_OF_LINES20=4878553
+
+
+# Size of WikEd : 24392765
+# 5%    - 1219638
+# 10% - 2439276
+# 20% - 4878553
+
+
+# In the same dir as the script there should exist Four Language Models with the following naming convention:
+# - in-source.blm (i.e. NUCLE)
+# - in-target.blm
+# - out-source.blm (i.e. WikEd)
+# - out-target.blm
+
+$MML_BI_SCRIPT/mml-score_mono.perl -corpus ./$WIKED_DIR/wiked.tc.clean -query ~/mosesdecoder/bin/query -input-extension err -output-extension cor | sort --parallel $NUM_OF_CORES -t $'\t' -k 2,2 -nr > score_sorted.out
+
+cat score_sorted.out | head -$NUM_OF_LINES5 > ./wiked_score5.out
+cat score_sorted.out | head -$NUM_OF_LINES10 > ./wiked_score10.out
+cat score_sorted.out | head -$NUM_OF_LINES20 > ./wiked_score20.out
+
+# Extract Top Scoring Sentences
+echo "Extracting Sentences from the corpus ................."
+python score_extract.py ./wiked_score5.out ./$WIKED_DIR/wiked.corpus ./wiked_pseudo5.corpus &
+python score_extract.py ./wiked_score10.out ./$WIKED_DIR/wiked.corpus ./wiked_pseudo10.corpus &
+python score_extract.py ./wiked_score20.out ./$WIKED_DIR/wiked.corpus ./wiked_pseudo20.corpus &
+wait
+
+python split_merge.py ./wiked_pseudo20.corpus ./wiked_pseudo20
+
+# Train a Language Model
+echo "Training a KenLM Language Model ................."
+$MOSES_BIN/lmplz -o 5 -S 80% -T /tmp < wiked_pseudo20.target > wiked_pseudo20.arpa
+
+# Binarize it
+echo "Binarizing Language Model ................."
+$MOSES_BIN/build_binary wiked_pseudo20.arpa wiked_pseudo20.blm
